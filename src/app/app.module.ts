aa120ac6f3fa17acf25d96c73848f0d90eae0691
David Preston <david.preston@onegas.com>
2021-02-01 14:36:47 -0600

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthButtonComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/auth-button/auth-button.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-button',
+  templateUrl: './auth-button.component.html',
+  styleUrls: ['./auth-button.component.scss']
+})
+export class AuthButtonComponent implements OnInit {
+
+  public gapiSetup = false; // marks if the gapi library has been loaded
+  public authInstance: gapi.auth2.GoogleAuth;
+  public error: string;
+  public user: gapi.auth2.GoogleUser;
+
+  async ngOnInit(): Promise<void> {
+    if (await this.checkIfUserAuthenticated()) {
+      this.user = this.authInstance.currentUser.get();
+    }
+  }
+
+  async initGoogleAuth(): Promise<void> {
+    // Create a new Promise where the resolve function is the callback
+    // passed to gapi.load
+    const pload = new Promise((resolve) => {
+      gapi.load('auth2', resolve);
+    });
+
+    // When the first promise resolves, it means we have gapi loaded
+    // and that we can call gapi.init
+    return pload.then(async () => {
+      await gapi.auth2
+        .init({ client_id: '584122374316-a3pd1dk57hpppd7i64r9vga19i2md73q.apps.googleusercontent.com' })
+        .then(auth => {
+          this.gapiSetup = true;
+          this.authInstance = auth;
+        });
+    });
+  }
+
+  async authenticate(): Promise<gapi.auth2.GoogleUser> {
+    // Initialize gapi if not done yet
+    if (!this.gapiSetup) {
+      await this.initGoogleAuth();
+    }
+
+    // Resolve or reject signin Promise
+    return new Promise(async () => {
+      await this.authInstance.signIn().then(
+        user => this.user = user,
+        error => this.error = error);
+    });
+  }
+
+  async checkIfUserAuthenticated(): Promise<boolean> {
+    // Initialize gapi if not done yet
+    if (!this.gapiSetup) {
+      await this.initGoogleAuth();
+    }
+
+    return this.authInstance.isSignedIn.get();
+  }
+
+  async logOut(): Promise<void> {
+    return new Promise<void>(async () => {
+      this.user = null;
+      this.error = null;
+    });
+  }
+}
